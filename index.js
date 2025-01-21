432abd83fe76a3164b8fdd3d702b482b76625fae
OkaChn <JunxiongChengCN@outlook.com>
2025-01-21 13:21:43 +0800

    config file optimization

--- index.js
+++ index.js
@@ -2,30 +2,65 @@ const express = require("express");
 const httpProxy = require("http-proxy");
 const cookieParser = require("cookie-parser");
 
-const path = require('path');
-const crypto = require('crypto');
+const crypto = require("crypto");
 const fs = require("fs");
+const path = require("path");
 const toml = require("toml");
 
-// Read TOML file
+
+const configFilePath = "config.toml";
+const exampleConfigFilePath = "config.toml.example";
+
+// Check if config.toml exists
+if (!fs.existsSync(configFilePath)) {
+    if (fs.existsSync(exampleConfigFilePath)) {
+        // Copy example file to config.toml
+        fs.copyFileSync(exampleConfigFilePath, configFilePath);
+        console.log(
+            `'${configFilePath}' is missing. The example file has been copied from '${exampleConfigFilePath}'. Please configure the file and restart the program.`
+        );
+    } else {
+        console.error(
+            "Both 'config.toml' and 'config.toml.example' are missing. Please provide a valid configuration file."
+        );
+    }
+    // Exit the program after copying the file or error
+    process.exit(1);
+}
+
 let data;
 let veri_page;
+let not_found_page;
+// Read TOML file
 try {
-    data = fs.readFileSync("config.toml", "utf8");
+    data = fs.readFileSync(configFilePath, "utf8");
 } catch (err) {
     console.error("Error reading file:", err);
+    process.exit(1);
 }
 
 try {
     veri_page = fs.readFileSync("public/verification.html", "utf8");
 } catch (err) {
     console.error("Error reading file:", err);
+    veri_page = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <h1>Verification</h1>
+    <script>
+        setTimeout(function () {
+            window.location.href = document.location.toString();
+        }, 5000);
+    </script>
+    </html>
+    `;
 }
 
 try {
     not_found_page = fs.readFileSync("public/404.html", "utf8");
 } catch (err) {
     console.error("Error reading file:", err);
+    not_found_page = "<h1>404 Not Found</h1>";
 }
 
 // Parse TOML file
@@ -34,21 +69,21 @@ try {
     config = toml.parse(data);
 } catch (parseError) {
     console.error("Parse TOML failed:", parseError);
-    return;
+    process.exit(1);
 }
 
 // Create Express app
 const app = express();
 const proxy = httpProxy.createProxyServer({});
-const port = config.port || 4000;
+const port = config.port;
 const targetUrl = config.target_url;
 const expired = config.expired;
 const secret_key = config.secret_key;
 
 function encryptToken(data) {
-    const hmac = crypto.createHmac('sha256', secret_key);
+    const hmac = crypto.createHmac("sha256", secret_key);
     hmac.update(data);
-    return hmac.digest('hex');
+    return hmac.digest("hex");
 }
 
 function verifyToken(data, token) {
@@ -72,7 +107,7 @@ function checkVerification(req, res, next) {
         res.cookie("validation_expiration", newExpirationTime, {
             maxAge: expired * 1000,
             httpOnly: true,
-            secure: true
+            secure: true,
         });
 
         // Generate the HTML for the verification page
@@ -81,8 +116,8 @@ function checkVerification(req, res, next) {
     }
 
     if (!verifyToken(expirationTime.toString(), validationToken)) {
-        res.clearCookie('validation_token');
-        res.clearCookie('validation_expiration');
+        res.clearCookie("validation_token");
+        res.clearCookie("validation_expiration");
         res.status(200).send(veri_page);
         return;
     }
@@ -94,7 +129,7 @@ function checkVerification(req, res, next) {
 app.use(checkVerification);
 
 // Proxy all requests
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
     proxy.web(req, res, { target: targetUrl }, (err) => {
         console.error(`Proxy error: Unable to connect to ${targetUrl}`);
         handleNotFound(req, res);
@@ -102,18 +137,18 @@ app.all('*', (req, res) => {
 });
 
 // Error handling for proxy server
-proxy.on('error', (err, req, res) => {
-    console.error('Proxy encountered an error:', err.message);
+proxy.on("error", (err, req, res) => {
+    console.error("Proxy encountered an error:", err.message);
     handleNotFound(req, res);
 });
 
 // Function to handle 404 responses
 function handleNotFound(req, res) {
     const ext = path.extname(req.path).toLowerCase();
-    if (!ext || ext === '.html') {
+    if (!ext || ext === ".html") {
         res.status(404).send(not_found_page);
     } else {
-        res.status(404).send('Not Found');
+        res.status(404).send("Not Found");
     }
 }
 
